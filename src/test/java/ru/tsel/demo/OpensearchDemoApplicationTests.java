7ede3370c73e4ae6cbb5e2324d2d9461d95c5b4f
a.tsel <a.tsel@i-sys.ru>
2025-07-10 17:35:56 +0400

    init commit

---
+++ src/test/java/ru/tsel/demo/OpensearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensearchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
